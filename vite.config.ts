4d8f422e7c2cff9350fee3bd30dfb3f6d9d0c496
Tharindu Devinda <tharindukodippili13@gmail.com>
2026-03-11 01:54:50 +0530

    errors fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://tharindudev13.github.io/Portfolio/'
+  base: '/Portfolio/'
 })
